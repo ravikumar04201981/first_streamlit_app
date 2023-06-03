85cbcb247ce31528f1452369a7edf29ae2c314a0
ravikumar04201981 <135448228+ravikumar04201981@users.noreply.github.com>
2023-06-03 21:44:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My parent new healty Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('A')
-steamlit.text('B')
+streamlit.text('B')
 streamlit.text('C')
